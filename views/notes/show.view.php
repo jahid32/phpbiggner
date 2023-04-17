4f51b3c6aef949d302a69230c56ea91582478586
Md  Mostafizur Rahman <jahid32@gmail.com>
2023-04-17 06:56:52 +0600

    finish curd

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -3,18 +3,16 @@
 <?php require base_path('views/partials/banner.php') ?>
 
 <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 flex gap-x-4 justify-end">
         <p class="mb-6">
             <a href="/notes" class="text-blue-500 underline">go back...</a>
         </p>
 
         <p><?= htmlspecialchars($note['body']) ?></p>
-
-        <form class="mt-6" method="POST">
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="id" value="<?= $note['id'] ?>">
-            <button class="text-sm text-red-500">Delete</button>
-        </form>
+        <footer class="mt-6">
+           <a class="inline-flex justify-center rounded-md border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" href="/note/edit?id=<?= $note['id']?>">Edit</a>  
+        </footer>
+       
     </div>
 </main>
 
